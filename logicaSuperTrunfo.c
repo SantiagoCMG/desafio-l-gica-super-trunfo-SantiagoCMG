ec9c6f1b76c4dd5a55823547f7069ec58e7f199b
Santiago <santiagocarlosm21@gmail.com>
2025-12-17 14:51:13 -0300

    Comparando Cartas do Super Trunfo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,83 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+typedef struct {
+    char nomeEstado[50];
+    char codigo[10];
+    int populacao;          // em milhões
+    float area;             // em mil km²
+    float pib;              // em trilhões
+    int pontosTuristicos;
+} Estado;
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
 
-    
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    Estado rj = {"Rio de Janeiro", "RJ", 17, 43.7, 1.1, 120};
+    Estado sp = {"Sao Paulo", "SP", 46, 248.2, 3.4, 180};
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    int opcao;
+    do {
+        printf("Escolha o atributo para comparar:\n");
+        printf("1 - Populacao\n");
+        printf("2 - Area\n");
+        printf("3 - PIB\n");
+        printf("4 - Pontos Turisticos\n");
+        printf("0 - Sair\n");
+        scanf("%d", &opcao);
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+        printf("\nComparando %s vs %s\n\n", rj.nomeEstado, sp.nomeEstado);
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+        switch (opcao) {
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+            case 1:
+                printf("Populacao (milhoes):\n");
+                printf("%s: %d\n", rj.nomeEstado, rj.populacao);
+                printf("%s: %d\n", sp.nomeEstado, sp.populacao);
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+                if (sp.populacao > rj.populacao)
+                    printf("Vencedor: %s\n", sp.nomeEstado);
+                else
+                    printf("Vencedor: %s\n", rj.nomeEstado);
+                break;
+
+            case 2:
+                printf("Area (mil km2):\n");
+                printf("%s: %.2f\n", rj.nomeEstado, rj.area);
+                printf("%s: %.2f\n", sp.nomeEstado, sp.area);
+
+                if (sp.area > rj.area)
+                    printf("Vencedor: %s\n", sp.nomeEstado);
+                else
+                    printf("Vencedor: %s\n", rj.nomeEstado);
+                break;
+
+            case 3:
+                printf("PIB (trilhoes):\n");
+                printf("%s: %.2f\n", rj.nomeEstado, rj.pib);
+                printf("%s: %.2f\n", sp.nomeEstado, sp.pib);
+
+                if (sp.pib > rj.pib)
+                    printf("Vencedor: %s\n", sp.nomeEstado);
+                else
+                    printf("Vencedor: %s\n", rj.nomeEstado);
+                break;
+
+            case 4:
+                printf("Pontos Turisticos:\n");
+                printf("%s: %d\n", rj.nomeEstado, rj.pontosTuristicos);
+                printf("%s: %d\n", sp.nomeEstado, sp.pontosTuristicos);
+
+                if (sp.pontosTuristicos > rj.pontosTuristicos)
+                    printf("Vencedor: %s\n", sp.nomeEstado);
+                else
+                    printf("Vencedor: %s\n", rj.nomeEstado);
+                break;
+            case 0:
+                printf("Saindo do programa...\n");
+                break;
+            default:
+                printf("Opcao invalida!\n");
+        }
+    }while (opcao != 0);
 
     return 0;
 }
